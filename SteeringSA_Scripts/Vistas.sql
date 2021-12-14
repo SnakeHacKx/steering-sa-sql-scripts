9f3b819b0f2045dad82eac542e02b1fded420acd
SnakeHacKx <omar.medina@utp.ac.pa>
2021-12-14 13:50:45 -0500

    Vistas editadas

--- SteeringSA_Scripts/Vistas.sql
+++ SteeringSA_Scripts/Vistas.sql
@@ -10,18 +10,20 @@ GO
 --VISTA DE GENERALES DEL SERVICIO (RELACIONA LAS TABLAS ASOCIADAS A UN SERVICIO)
 ALTER VIEW V_GENERALES_DE_SERVICIO
 AS
-	SELECT S.Cod_Servicio AS 'Codigo',C.Nombre+' '+C.Apellido AS 'Conductor',
-	V.Placa'Placa de vehiculo',V.Tipo AS 'Tipo de vehiculo',
-	C.Cedula AS 'Cedula de Conductor',
-	V.Color AS 'Color de Vehiculo',T.Nombre_servicio AS'Tipo de servicio',
-	T.Cod_tipo_servicio AS 'Código de tipo'
-	,FORMAT(S.Fecha_inicio,'dd-MM-yyyy') AS 'Fecha de inicio',
-	FORMAT(S.Fecha_finalizacion,'dd-MM-yyyy') AS 'Fecha de finalizacion',
-	ROUND(S.Monto_Total_Servicio,2,1) AS 'Costo total',
+	SELECT S.Cod_Servicio AS 'ID',
+	T.Nombre_servicio AS'Tipo de Servicio',
+	T.Cod_tipo_servicio AS 'ID Tipo de Servicio',
+	C.Nombre+' '+C.Apellido AS 'Conductor',
+	C.Cedula AS 'Cédula (Conductor)',
 	CL.Nombre_Cliente+' '+CL.Apellido_Cliente AS 'Cliente',
-	CL.Cedula_Cliente AS 'Cedula de Cliente',
-	DATEDIFF(DAY,S.Fecha_inicio,S.Fecha_finalizacion) AS 'Duracion',
-	S.Descripcion_servicio AS 'Descripcion'
+	CL.Cedula_Cliente AS 'Cédula (Cliente)',
+	V.Placa AS 'Matrícula del Vehículo',
+	V.Tipo AS 'Tipo de Vehículo',
+	FORMAT(S.Fecha_inicio,'dd-MM-yyyy') AS 'Fecha de Inicio',
+	FORMAT(S.Fecha_finalizacion,'dd-MM-yyyy') AS 'Fecha de Finalización',
+	DATEDIFF(DAY,S.Fecha_inicio,S.Fecha_finalizacion) AS 'Duración (días)',
+	ROUND(S.Monto_Total_Servicio,2,1) AS 'Costo Total',
+	S.Descripcion_servicio AS 'Descripción'
 	FROM Servicio S
 	INNER JOIN Conductor C ON S.Cedula_Conductor=C.Cedula
 	INNER JOIN Vehiculo V ON S.Placa = V.Placa
@@ -32,12 +34,14 @@ GO
 --VISTA DE GENERALES DEL MANTENIMIENTO (RELACIONA LAS TABLAS ASOCIADAS A UN MANTENIMIENTO)
 ALTER VIEW V_GENERALES_DE_MANTENIMIENTO
 AS
-	SELECT M.Cod_Mantenimiento AS 'Codigo de mantenimiento',
-	V.Placa AS 'Placa de vehiculo',V.Tipo AS 'Tipo de vehiculo',
-	M.Cod_reporte AS 'Reporte',
-	FORMAT(M.Fecha,'dd-MM-yyyy') AS 'Fecha de realizacion',M.Costo AS 'Costo total',
+	SELECT M.Cod_Mantenimiento AS 'ID',
+	M.Cod_reporte AS 'ID Reporte',
+	M.Costo AS 'Costo Total',
 	M.Estado AS 'Estado Actual',
-	M.Descripcion
+	FORMAT(M.Fecha,'dd-MM-yyyy') AS 'Fecha de Realización',
+	V.Placa AS 'Matrícula de Vehículo',
+	V.Tipo AS 'Tipo de Vehículo',
+	M.Descripcion AS 'Descripción'
 	FROM Mantenimiento M
 	INNER JOIN Vehiculo V ON V.Placa = M.Placa_Vehiculo
 GO
@@ -45,50 +49,68 @@ GO
 --VISTA GENERALES DE CLIENTE
 ALTER VIEW V_GENERALES_DE_CLIENTE
 AS
-	SELECT Cedula_Cliente AS 'N° Cedula',
-	Nombre_Cliente AS 'Nombre',Apellido_Cliente AS 'Apellido',
-	FORMAT(Fecha_Nacimiento_Cliente,'dd-MM-yyyy') AS 'Fecha de nacimiento',
+	SELECT Cedula_Cliente AS 'N° Cédula',
+	Nombre_Cliente AS 'Nombre',
+	Apellido_Cliente AS 'Apellido',
+	FORMAT(Fecha_Nacimiento_Cliente,'dd-MM-yyyy') AS 'Fecha de Nacimiento',
 	YEAR(GETDATE())-YEAR(Fecha_Nacimiento_Cliente) AS 'Edad',
-	Telefono_Cliente AS 'Telefono',Direccion_CLiente AS 'Direccion' FROM TB_Cliente
+	Telefono_Cliente AS 'Contacto',
+	Direccion_CLiente AS 'Dirección' 
+	FROM TB_Cliente
 GO
 --VISTA GENERALES DE CONDUCTOR
 ALTER VIEW V_GENERALES_DE_CONDUCTOR
 AS
-	SELECT Cedula AS 'N° Cedula',Nombre,Apellido,Telefono AS 'Contacto',
-	FORMAT(Fecha_de_nacimiento,'dd-MM-yyyy') AS 'Fecha de nacimiento',
+	SELECT Cedula AS 'N° Cédula',
+	Nombre,
+	Apellido,
+	Telefono AS 'Contacto',
+	FORMAT(Fecha_de_nacimiento,'dd-MM-yyyy') AS 'Fecha de Nacimiento',
 	YEAR(GETDATE()) -YEAR(Fecha_de_nacimiento)AS 'Edad',
-	Tipo_de_sangre AS 'Grupo sanguineo',Tipo_de_licencia AS 'Licencia'
+	Tipo_de_sangre AS 'Grupo Sanguíneo',
+	Tipo_de_licencia AS 'Tipo de Licencia'
 	FROM Conductor
 GO
 
 --VISTA DE GENERALES DE VEHICULO
-CREATE VIEW V_GENERALES_DE_VEHICULO
+ALTER VIEW V_GENERALES_DE_VEHICULO
 AS
-	SELECT Placa AS 'Placa de vehiculo',Tipo,Modelo_vehiculo AS 'Modelo de Vehiculo',
-	pasajero AS 'Capacidad',Color,Tipo_de_combustible AS 'Tipo de combustible',Estado FROM Vehiculo
+	SELECT Placa AS 'Matrícula',
+	Tipo,
+	Modelo_vehiculo AS 'Modelo',
+	pasajero AS 'Capacidad',
+	Color,
+	Tipo_de_combustible AS 'Tipo de Combustible',
+	Estado 
+	FROM Vehiculo
 GO
 
 --VISTA DE GENERALES DE REPORTE
 ALTER VIEW V_GENERALES_DE_REPORTE
 AS
-	SELECT Cod_reporte AS 'Codigo de Reporte',Placa_Vehiculo as 'Placa del vehiculo'
-	,FORMAT(Fecha,'dd-MM-yyyy') AS 'Fecha de reporte', Estado,
-	Descripcion FROM Reporte
+	SELECT Cod_reporte AS 'ID',
+	Placa_Vehiculo as 'Matrícula del Vehículo'
+	,FORMAT(Fecha,'dd-MM-yyyy') AS 'Fecha de Registro',
+	Estado,
+	Descripcion AS 'Descripción' FROM Reporte
 GO
 
 --VISTA GENERAL DE TIPO DE SERVICIO
 ALTER VIEW V_GENERALES_DE_TIPO_DE_SERVICIO
 AS
-	SELECT Cod_tipo_servicio AS 'Codigo',Nombre_servicio AS 'Nombre del servicio',
-	ROUND(Costo_servicio,2,1) AS 'Costo diario' FROM Tipo_servicios
+	SELECT Cod_tipo_servicio AS 'ID',
+	Nombre_servicio AS 'Nombre',
+	ROUND(Costo_servicio,2,1) AS 'Costo Diario'
+	FROM Tipo_servicios
 GO
 
 --VISTAR GENERAL DE HISTORIAL DE ACCIONES SOBRE LA BASE DE DATOS
 ALTER VIEW V_VER_HISTORIAL_DE_ACCIONES
 AS
 	SELECT U.Usuario AS 'Nombre de Usuario',
-	U.Rol AS 'Rol de Usuario',H.Accion AS 'Accion realizada',
-	FORMAT(H.Fecha,'dd-MM-yyyy') AS 'Fecha de realizacion',H.ID_operacion'ID de accion' 
+	U.Rol AS 'Rol',H.Accion AS 'Acción Realizada',
+	FORMAT(H.Fecha,'dd-MM-yyyy') AS 'Fecha de Realización',
+	H.ID_operacion'ID de Acción'
 	FROM TB_Historial H
 	INNER JOIN V_VER_USUARIOS U ON U.Usuario=H.Nombre_usuario
 GO
